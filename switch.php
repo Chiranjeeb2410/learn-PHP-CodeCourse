533fb96feeb4d70fabea1558abc8ed042eed279f
chiranjeeb2410 <chiranjeebm2410@gmail.com>
2017-02-02 07:19:42 -0500

    committing switch tutorials

---
+++ switch.php
@@ -0,0 +1,13 @@
+<?php
+
+switch (2) {
+	case '1':
+		# code...
+		echo "The value is 1";
+		break;
+	
+	default:
+		# code...
+		echo "The value is unknown";
+		break;
+}
---
+++ switch2.php
@@ -0,0 +1,22 @@
+<?php
+
+$weather = 'sunny';
+
+$color = null;
+
+switch (true) {
+	
+	case $weather == 'sunny';
+		# code...
+		$color = 'blue';
+		break;
+	
+	case $weather == 'cloudy';
+		# code...
+		$color = 'grey';
+		break;
+
+}
+
+echo $color;
+	
